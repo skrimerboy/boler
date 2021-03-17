5086b086cee69746afdf490c984b5ad89e61887a
skrimeboy <napoleon1812russian@gmail.com>
2021-03-17 18:35:46 +0300

    new project js, but it don't working

---
+++ js/script.js
@@ -0,0 +1,33 @@
+prompt ('сколько денег у кевина?')
+"totalCash"
+prompt ('сколько часов у кевина?')
+"watchesCount"
+let watchesSum = 1
+undefined
+typeof watchesSum
+"number"
+prompt ( 'сколько у Кевина сережек?')
+"earringsCount"
+let earringsSum = 1
+undefined
+prompt ('сколько стоят одни часы?')
+"earringsSum"
+prompt ('сколько стоят одни сережки?')
+"earringsSum"
+prompt ('сколько стоят одни часы?')
+"watchesSum"
+prompt ('сколько стоят сережки и часы?')
+"watchesSum"
+prompt ('сколько стоят сережки и часы?')
+"watchesSum+earringsSum"
+prompt ('сколько кевин может купить часов?')
+"watchesCount"
+prompt ('сколько кевин может купить сережек?')
+"earringsCount"
+
+alert ('  сколько часов у кевина?' )
+
+typeof totalCash
+"number"
+typeof totalCash
+"number"
